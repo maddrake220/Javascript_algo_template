7d6a67bc2f88232c0a32d64d8613a9a1b5e5f6c0
최재원 <83238246+maddrake220@users.noreply.github.com>
2022-01-21 20:12:40 +0900

    Update BOJ_정렬_18870.js

--- BOJ_정렬_18870.js
+++ BOJ_정렬_18870.js
@@ -19,3 +19,28 @@ function compCoord() {
 }
 
 console.log(compCoord())
+
+
+//// map을 이용하여 수정
+
+const [, ...input] = require('fs').readFileSync('./input.txt').toString().trim().split('\n');
+
+function compCoord() {
+  const arr = input[0]
+    .split(' ')
+    .map(v => Number(v))
+
+  const set = [...new Set(arr)].sort((a,b) => a - b)
+  const answer = []
+  const map = new Map();
+
+  set.forEach((value, index) => map.set(value, index))
+  
+  for(i in arr) {
+   answer.push(map.get(arr[i]))
+  }
+ 
+  return answer.join(' ')
+}
+
+console.log(compCoord())
